c37570e6572e38b0ef970cc24524c0646b47564d
Scott Rod <djmtype@gmail.com>
2022-07-27 19:34:06 -0400

    Tweak scheme button

--- postcss.config.js
+++ postcss.config.js
@@ -7,6 +7,14 @@ module.exports = {
 		postcssPresetEnv({
 			stage: 0,
 			features: {
+				'logical-properties-and-values': false, 
+        'prefers-color-scheme-query': false, 
+        'gap-properties': false,
+        'custom-properties': false,
+        'dir-pseudo-class': false,
+        'focus-within-pseudo-class': false,
+        'focus-visible-pseudo-class': false,
+        'color-functional-notation': false,
 				"custom-media-queries": {
 					importFrom: [
 						"./node_modules/open-props/src/props.media.css",
