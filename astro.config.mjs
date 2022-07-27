21aff93fea35dac20758e33c257f2a47b82929e9
Scott Rod <djmtype@gmail.com>
2022-07-27 12:46:56 -0400

    update schemes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import sitemap from "@astrojs/sitemap";
 
 import { astroImageTools } from "astro-imagetools";
 
+
+
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://astro-xenon.netlify.app",
