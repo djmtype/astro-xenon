0b4c400da9ad0e5ec90b5158ec0e125d46b48d8c
Scott Rod <djmtype@gmail.com>
2022-10-19 12:03:28 -0400

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,6 +22,9 @@ export default defineConfig({
 	},
 	markdown: {
 		drafts: true,
+		
+		extendDefaultPlugins: true,
+	
 		// Can be 'shiki' (default), 'prism' or false to disable highlighting
 		syntaxHighlight: "shiki",
 		shikiConfig: {
